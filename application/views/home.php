976b46704b5ba301567506469b7e2114f2a3d54c
Flighttrajectory <robindra22@gmail.com>
2016-05-24 20:01:25 +0700

    Perubahan Form Budget
    Menghilangkan spinner pada form budget

--- application/views/home.php
+++ application/views/home.php
@@ -98,7 +98,6 @@
                                         <div class="form-group">
                                             <label class="control-label col-md-3 col-sm-3 col-xs-12">Budget</label>
                                             <div class="col-md-9 col-sm-9 col-xs-12">
-                                                <input type="text" name="country" id="autocomplete-custom-append" class="form-control col-md-10" style="float: left;" />
                                             </div>
                                         </div>
                                         <div class="form-group">
--- application/views/home2.php
+++ application/views/home2.php
@@ -130,7 +130,7 @@
                                         <div class="form-group">
                                             <label class="control-label col-md-3 col-sm-3 col-xs-12">Budget</label>
                                             <div class="col-md-9 col-sm-9 col-xs-12">
-                                                <input type="text" name="country" id="autocomplete-custom-append" class="form-control col-md-10" style="float: left;" />
+                                                <input type="number" name="country" id="autocomplete-custom-append" class="form-control col-md-10" style="float: left;" />
                                                 <div id="autocomplete-container" style="position: relative; float: left; width: 400px; margin: 10px;">
                                                 </div>
                                             </div>

5917589cca80c713eebf8058f676dcd52dabf9bd
Flighttrajectory <robindra22@gmail.com>
2016-05-24 08:17:16 +0700

    Penambahan Colouser Pada Home
    Gambar di perbanyak , sebelumnya ada error / miss

--- application/views/home.php
+++ application/views/home.php
@@ -93,8 +93,9 @@
             <li><span>Image 01</span><div></div></li>
             <li><span>Image 02</span><div></div></li>
             <li><span>Image 03</span><div></div></li>
-            <li><span>Image 04</span><div></h3></div></li>
-                            
+            <li><span>Image 04</span><div></div></li>
+            <li><span>Image 05</span><div></div></li>
+            <li><span>Image 06</span><div></div></li>               
         </ul>
         <section class="content right_sidebar">
             <div class="container">

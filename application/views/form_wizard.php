40ca247c703019bef61d505b54e121de97d3df2d
haidardzaky <haidardz@gmail.com>
2016-06-01 16:47:11 +0700

    wizard penginapan
    wizard penginapan update

--- application/views/form_wizard.php
+++ application/views/form_wizard.php
@@ -107,27 +107,63 @@
                                             <div id="step-2">
                                                 <p class="text-center">
                                                     <form action="action_page.php">
-														<div class="field-wrap">
-															<label>
-																Lokasi <span class="req"> :</span>
-															</label>
-															<input type="email"required autocomplete="off"/>
-													    </div>
-														<div class="field-wrap">
-															<label>
-																Hotel <span class="req"> </span>
-															</label>
-															<input type="email"required autocomplete="off"/>
-													    </div>
+																											
+														<div class="container">
+														<h2>PILIH HOTEL</h2>
+														  <form class="form-horizontal" role="form">
+															<div class="form-group">
+															  <label class="col-sm-2 control-label">Lokasi</label>
+															  <div class="col-sm-4">
+																<input class="form-control" id="focusedInput" type="text" value="" placeholder="Lokasi">
+															  </div>
+															</div>
+														</div>
+														
+														<div class="container">
+															<form class="form-horizontal" role="form">
+															<div class="form-group">
+															  <label class="col-sm-2 control-label">Hotel</label>
+															  <div class="col-sm-4">
+																<input class="form-control" id="focusedInput" type="text" value="" placeholder="Hotel">
+															  </div>
+															</div>
+														</div>
+														
+														<div class="container">
+															<form class="form-horizontal" role="form">
+															<div class="form-group">
+																<label class="col-sm-2 control-label">Check In</label>
+																<div class="col-sm-4">
+																	<input style="padding-left: 40px;" type="text" class="form-control has-feedback-left" id="single_cal4" placeholder="MM/DD/YYYY" aria-describedby="inputSuccess2Status4">
+																	<span class="fa fa-calendar-o form-control-feedback left" aria-hidden="true"></span>
+																	<span id="inputSuccess2Status4" class="sr-only">(success)</span>
+																</div>
+															</div>
+														</div>
+														
+														<div class="container">
+															<form class="form-horizontal" role="form">
+															<div class="form-group">
+																<label class="col-sm-2 control-label">Check Out</label>
+																<div class="col-sm-4">
+																	<input style="padding-left: 40px;" type="text" class="form-control has-feedback-left" id="single_cal4" placeholder="MM/DD/YYYY" aria-describedby="inputSuccess2Status4">
+																	<span class="fa fa-calendar-o form-control-feedback left" aria-hidden="true"></span>
+																	<span id="inputSuccess2Status4" class="sr-only">(success)</span>
+																</div>
+															</div>
+														</div>
 														 
-														  <input type="submit" value="Submit">
+														  
+													
+													<br>
+													<div class="col-md-5" align="right">	
+														<input type="submit" value="Submit" style="background-color:orange">
+													</div>
 													</form> 
                                                 </p>
-                                                <p class="clearfix text-center">
-                                                    <button class="buttonPrevious btn btn-add" data-toggle="modal" data-target="#myModal">Skip</button>
-                                                    <button class="buttonPrevious btn btn-add">Add</button>
-                                                </p>
+                                                
                                             </div>
+											
                                             <div id="step-3">
                                                 <p class="text-center">
                                                     -- Kosong --
@@ -248,6 +284,9 @@
             </div>
         </div>
     </div>
+	<?php 
+		$this->load->view('script/date_picker');
+	?>
     <!-- End Modal -->
 </body>
 

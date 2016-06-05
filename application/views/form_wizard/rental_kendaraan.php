99f348e62ee4e685b38a746537be0e209f09cd49
fuyudi <suyudifikri@gmail.com>
2016-06-06 00:34:21 +0700

    Update Form Rental Kendaraan
    Penambahan form untuk rental mobil

---
+++ application/views/form_wizard/rental_kendaraan.php
@@ -0,0 +1,65 @@
+<!-- col -->
+<div class="col-md-12 col-sm-12 col-xs-12">
+    <!-- x_panel -->
+    <div class="x_panel">
+        <!-- x_content -->
+        <div class="x_content">
+            <!-- tab panel -->
+            <div class="" role="tabpanel" data-example-id="togglable-tabs">
+				<!-- tab content -->
+                <div id="myTabContent" class="tab-content">
+                    <div role="tabpanel" class="tab-pane fade active in" id="pesawat" aria-labelledby="home-tab">
+                        <div class="col-md-12 col-sm-12 col-xs-12">
+                                <!-- x_content -->
+                                <div class="x_content">
+                                    <!-- form group -->
+                                    <div class="form-group">
+                                        <label class="control-label col-md-12 col-sm-12 col-xs-12">Kota Asal</label>
+                                        <div class="col-md-12 col-sm-12 col-xs-12 form-group has-feedback">
+                                            <input type="text" class="form-control has-feedback-left" placeholder="Bandung">
+                                        </div>
+                                    </div>
+                                    <!-- end from group -->
+                                </div>
+                                <!-- end x_content -->
+                        </div>
+                        <!-- end tujuan penerbangan -->
+                        <!-- waktu penerbangan -->
+                        <div class="col-md-12 col-sm-12 col-xs-12">
+                                <!-- x_content -->
+                                <div class="x_content">
+                                    <!-- tanggal -->
+                                    <div class="form-group">
+                                        <label class="control-label col-md-4 col-sm-4 col-xs-4">Tanggal Peminjaman</label>
+										<label class="control-label col-md-4 col-sm-4 col-xs-4">Lama Peminjaman</label>
+										<label class="control-label col-md-4 col-sm-4 col-xs-4">Jumlah Mobil</label>
+                                        <div class="col-md-4 col-sm-4 col-xs-12">
+                                            <input style="padding-left: 40px;" type="text" class="form-control has-feedback-left" id="single_cal4" placeholder="DD/MM/YYYY" aria-describedby="inputSuccess2Status4">
+                                            <span class="fa fa-calendar-o form-control-feedback left" aria-hidden="true"></span>
+                                        </div>
+										<div class="col-md-4 col-sm-4 col-xs-12 form-group has-feedback">
+                                            <input type="text" class="form-control has-feedback-left" id="inputSuccess2" placeholder="0">
+                                        </div>
+										<div class="col-md-4 col-sm-4 col-xs-12 form-group has-feedback">
+                                            <input type="text" class="form-control has-feedback-left" id="inputSuccess2" placeholder="0">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4 col-sm-4 col-xs-12 text-center">
+                                        <button class="btn btn-success">Cari Tiket</button>
+                                    </div>
+                                </div>
+                                <!-- end x_content -->
+                        </div>
+                        <!-- end waktu penerbangan -->
+                        <div class="clearfix"></div>
+                    </div>
+                </div>
+                <!-- end tab content -->
+            </div>
+            <!-- end tab panel -->
+        </div>
+        <!-- end x_content -->
+    </div>
+    <!-- end x_panel -->
+</div>
+<!-- end col -->

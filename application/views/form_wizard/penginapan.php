bbea55774c20006808b2282fce6aa0242e082865
haidardzaky <haidardz@gmail.com>
2016-07-25 15:17:09 +0700

    penginapan edit wizard
    rubah tampilan form penginapan

--- application/views/form_wizard/penginapan.php
+++ application/views/form_wizard/penginapan.php
@@ -1,49 +1,46 @@
 <p class="text-center">
     <form action="#">
         <div class="container">
-            <h2>PILIH HOTEL</h2>
+            <h2>PILIH PENGINAPAN</h2>
             <form class="form-horizontal" role="form">
                 <div class="form-group">
-                    <label class="col-sm-2 control-label">Lokasi</label>
-                    <div class="col-sm-4">
+                    <label class="col-sm-6 control-label">Lokasi</label>
+					<label class="col-sm-6 control-label">Hotel</label>
+                </div>
+				<div class="col-sm-6">
                         <input class="form-control" id="focusedInput" type="text" value="" placeholder="Lokasi">
-                    </div>
                 </div>
-        </div>
-        <div class="container">
-            <form class="form-horizontal" role="form">
-                <div class="form-group">
-                    <label class="col-sm-2 control-label">Hotel</label>
-                    <div class="col-sm-4">
+				<div class="col-sm-6">
                         <input class="form-control" id="focusedInput" type="text" value="" placeholder="Hotel">
-                    </div>
                 </div>
         </div>
+		
+		<br>
+
         <div class="container">
             <form class="form-horizontal" role="form">
                 <div class="form-group">
-                    <label class="col-sm-2 control-label">Check In</label>
-                    <div class="col-sm-4">
-                        <input style="padding-left: 40px;" type="text" class="form-control has-feedback-left" id="single_cal4" placeholder="MM/DD/YYYY" aria-describedby="inputSuccess2Status4">
-                        <span class="fa fa-calendar-o form-control-feedback left" aria-hidden="true"></span>
-                        <span id="inputSuccess2Status4" class="sr-only">(success)</span>
-                    </div>
+                    <label class="col-sm-6 control-label">Check In</label>
+					<label class="col-sm-6 control-label">Check Out</label>
+				</div>
+                <div class="col-sm-6">
+                    <input style="padding-left: 40px;" type="text" class="form-control has-feedback-left" id="single_cal4" placeholder="MM/DD/YYYY" aria-describedby="inputSuccess2Status4">
+                    <span class="fa fa-calendar-o form-control-feedback left" aria-hidden="true"></span>
+                    <span id="inputSuccess2Status4" class="sr-only">(success)</span>
                 </div>
-        </div>
-        <div class="container">
-            <form class="form-horizontal" role="form">
-                <div class="form-group">
-                    <label class="col-sm-2 control-label">Check Out</label>
-                    <div class="col-sm-4">
-                        <input style="padding-left: 40px;" type="text" class="form-control has-feedback-left" id="single_cal4" placeholder="MM/DD/YYYY" aria-describedby="inputSuccess2Status4">
-                        <span class="fa fa-calendar-o form-control-feedback left" aria-hidden="true"></span>
-                        <span id="inputSuccess2Status4" class="sr-only">(success)</span>
-                    </div>
+				<div class="col-sm-6">
+                    <input style="padding-left: 40px;" type="text" class="form-control has-feedback-left" id="single_cal4" placeholder="MM/DD/YYYY" aria-describedby="inputSuccess2Status4">
+                    <span class="fa fa-calendar-o form-control-feedback left" aria-hidden="true"></span>
+                    <span id="inputSuccess2Status4" class="sr-only">(success)</span>
                 </div>
+               
         </div>
+
         <br>
-        <div class="col-md-5" align="right">
-            <input type="submit" value="Submit" style="background-color:orange">
+        <div class="container">
+			<div class="col-sm-12 text-right">
+				<button class="btn btn-success">Cari</button>
+			</div>
         </div>
         </form>
 </p>

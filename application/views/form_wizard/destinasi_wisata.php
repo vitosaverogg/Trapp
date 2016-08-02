e22abeaab0c8e77d7f84c75c3d5acf54d6030aad
sandydaud <sandydaud62@gmail.com>
2016-08-02 14:11:03 +0700

    destinasi
    penambahan fitur destinasi

--- application/views/form_wizard/destinasi_wisata.php
+++ application/views/form_wizard/destinasi_wisata.php
@@ -11,26 +11,49 @@
                     <div role="tabpanel" class="tab-pane fade active in" id="pesawat" aria-labelledby="home-tab">
                         <!-- waktu penerbangan -->
                         <div class="col-md-12 col-sm-12 col-xs-12">
-                                <!-- x_content -->
-                                <div class="x_content">
-                                    <!-- tanggal -->
-                                    <div class="form-group">
-										<label class="control-label col-md-6 col-sm-6 col-xs-6">Lokasi</label>
-										<label class="control-label col-md-6 col-sm-6 col-xs-6">Tanggal</label>
-                                        
-										<div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-                                            <input type="text" class="form-control has-feedback-left" id="inputSuccess2" placeholder="0">
-                                        </div>
-										<div class="col-md-6 col-sm-6 col-xs-12">
-                                            <input style="padding-left: 40px;" type="text" class="form-control has-feedback-left" id="single_cal4" placeholder="DD/MM/YYYY" aria-describedby="inputSuccess2Status4">
-                                            <span class="fa fa-calendar-o form-control-feedback left" aria-hidden="true"></span>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-4 col-sm-4 col-xs-12">
-                                        <button class="btn btn-success">Cari Event</button>
-                                    </div>
-                                </div>
-                                <!-- end x_content -->
+                            <div class="col-md-4 col-sm-4 col-xs-4">
+                                <img src="<?php echo base_url(); ?>assets/images/paris.jpg" class="img-responsive" alt="125x125">
+                                <h2>Paris - Europe</h2>
+                                <h5>A town which has Eiffel Tower as an icon of Paris. It has many spot for photograpy.The Eiffel Tower is a wrought iron lattice tower on the Champ de Mars in Paris, France. It is named after the engineer Gustave Eiffel, whose company designed and built the tower. </h5>
+                                <button class="btn btn-success">Read More</button>
+                                <button class="btn btn-success">Choose</button>
+                            </div>
+                            <div class="col-md-4 col-sm-4 col-xs-4">
+                                <img src="<?php echo base_url(); ?>assets/images/destinasi/universal.jpg" class="img-responsive" alt="125x125">
+                                <h2>Universal Studio - Singapore</h2>
+                                <h5>Universal Studios Singapore is 20 hectares (49 acres) in size, occupying the easternmost part of the 49-hectare (120-acre) Resorts World Sentosa. There are a total of 24 attractions, of which 18 are original or specially adapted for the park. The park consists of seven themed zones which surround a lagoon.</h5>
+                                <button class="btn btn-success">Read More</button>
+                                <button class="btn btn-success">Choose</button>
+                            </div>
+                            <div class="col-md-4 col-sm-4 col-xs-4">
+                                <img src="<?php echo base_url(); ?>assets/images/destinasi/bali.jpg" class="img-responsive" alt="125x125">
+                                <h2>Bali - Indonesia</h2>
+                                <h5>Bali is an Indonesian island known for its forested volcanic mountains, iconic rice paddies, beaches and coral reefs. The island is home to religious sites such as cliffside Uluwatu Temple. To the south, the beachside city of Kuta has lively bars, while Seminyak, Sanur and Nusa Dua are popular resort towns. The island is also known for its yoga and meditation retreats</h5>
+                                <button class="btn btn-success">Read More</button>
+                                <button class="btn btn-success">Choose</button>
+                            </div>
+                            <br></br>
+                            <div class="col-md-4 col-sm-4 col-xs-4">
+                                <img src="<?php echo base_url(); ?>assets/images/destinasi/borobudur.jpg" class="img-responsive" alt="125x125">
+                                <h2>Candi Borobudur - Indonesia</h2>
+                                <h5>Borobudur, or Barabudur, is a 9th-century Mahayana Buddhist Temple in Magelang, Central Java, Indonesia. The monument consists of nine stacked platforms, six square and three circular, topped by a central dome.</h5>
+                                <button class="btn btn-success">Read More</button>
+                                <button class="btn btn-success">Choose</button>
+                            </div>
+                            <div class="col-md-4 col-sm-4 col-xs-4">
+                                <img src="<?php echo base_url(); ?>assets/images/destinasi/disneyland.jpg" class="img-responsive" alt="125x125">
+                                <h2>Disneyland - Hongkong</h2>
+                                <h5>Hong Kong Disneyland is a theme park located on reclaimed land in Penny's Bay, Lantau Island. It is the first theme park located inside the Hong Kong Disneyland Resort and is owned and managed by the Hong Kong International Theme Parks.</h5>
+                                <button class="btn btn-success">Read More</button>
+                                <button class="btn btn-success">Choose</button>
+                            </div>
+                            <div class="col-md-4 col-sm-4 col-xs-4">
+                                <img src="<?php echo base_url(); ?>assets/images/destinasi/greatwall.jpg" class="img-responsive" alt="125x125">
+                                <h2>Great Wall - China</h2>
+                                <h5>The Great Wall of China is a series of fortifications made of stone, brick, tamped earth, wood, and other materials, generally built along an east-to-west line across the historical northern borders of China to protect the Chinese states and empires against the raids and invasions of the various nomadic groups of the Eurasian Steppe.</h5>
+                                <button class="btn btn-success">Read More</button>
+                                <button class="btn btn-success">Choose</button>
+                            </div>
                         </div>
                         <!-- end waktu penerbangan -->
                         <div class="clearfix"></div>

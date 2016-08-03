59d00c7a465ecb54304abb4ca8b972cc6b58e0f8
sandydaud <sandydaud62@gmail.com>
2016-08-03 22:07:29 +0700

    tambahan tampilan
    perlengkapan dari destinasi wisata

--- application/views/form_wizard/destinasi_wisata.php
+++ application/views/form_wizard/destinasi_wisata.php
@@ -13,23 +13,25 @@
                         <div class="col-md-12 col-sm-12 col-xs-12">
                             <div class="col-md-4 col-sm-4 col-xs-4">
                                 <img src="<?php echo base_url(); ?>assets/images/paris.jpg" class="img-responsive" alt="125x125">
-                                <h2>Paris - Europe</h2>
+                                <h2>Paris (Eiffel Tower) - Europe</h2>
                                 <h5>A town which has Eiffel Tower as an icon of Paris. It has many spot for photograpy.The Eiffel Tower is a wrought iron lattice tower on the Champ de Mars in Paris, France. It is named after the engineer Gustave Eiffel, whose company designed and built the tower. </h5>
-                                <button class="btn btn-success">Read More</button>
+                                <div class="btn-group" role="group" style="float: right; margin-right: 5px;">                                
+                                <button class="btn btn-default" data-toggle="modal" data-target="#paris">Read More</button>
                                 <button class="btn btn-success">Choose</button>
+                            </div>
                             </div>
                             <div class="col-md-4 col-sm-4 col-xs-4">
                                 <img src="<?php echo base_url(); ?>assets/images/destinasi/universal.jpg" class="img-responsive" alt="125x125">
                                 <h2>Universal Studio - Singapore</h2>
                                 <h5>Universal Studios Singapore is 20 hectares (49 acres) in size, occupying the easternmost part of the 49-hectare (120-acre) Resorts World Sentosa. There are a total of 24 attractions, of which 18 are original or specially adapted for the park. The park consists of seven themed zones which surround a lagoon.</h5>
-                                <button class="btn btn-success">Read More</button>
+                                <button class="btn btn-default" data-toggle="modal" data-target="#universal">Read More</button>
                                 <button class="btn btn-success">Choose</button>
                             </div>
                             <div class="col-md-4 col-sm-4 col-xs-4">
                                 <img src="<?php echo base_url(); ?>assets/images/destinasi/bali.jpg" class="img-responsive" alt="125x125">
                                 <h2>Bali - Indonesia</h2>
                                 <h5>Bali is an Indonesian island known for its forested volcanic mountains, iconic rice paddies, beaches and coral reefs. The island is home to religious sites such as cliffside Uluwatu Temple. To the south, the beachside city of Kuta has lively bars, while Seminyak, Sanur and Nusa Dua are popular resort towns. The island is also known for its yoga and meditation retreats</h5>
-                                <button class="btn btn-success">Read More</button>
+                                <button class="btn btn-default" data-toggle="modal" data-target="#bali">Read More</button>
                                 <button class="btn btn-success">Choose</button>
                             </div>
                             <br></br>
@@ -37,21 +39,21 @@
                                 <img src="<?php echo base_url(); ?>assets/images/destinasi/borobudur.jpg" class="img-responsive" alt="125x125">
                                 <h2>Candi Borobudur - Indonesia</h2>
                                 <h5>Borobudur, or Barabudur, is a 9th-century Mahayana Buddhist Temple in Magelang, Central Java, Indonesia. The monument consists of nine stacked platforms, six square and three circular, topped by a central dome.</h5>
-                                <button class="btn btn-success">Read More</button>
+                                <button class="btn btn-default" data-toggle="modal" data-target="#borobudur">Read More</button>
                                 <button class="btn btn-success">Choose</button>
                             </div>
                             <div class="col-md-4 col-sm-4 col-xs-4">
                                 <img src="<?php echo base_url(); ?>assets/images/destinasi/disneyland.jpg" class="img-responsive" alt="125x125">
                                 <h2>Disneyland - Hongkong</h2>
                                 <h5>Hong Kong Disneyland is a theme park located on reclaimed land in Penny's Bay, Lantau Island. It is the first theme park located inside the Hong Kong Disneyland Resort and is owned and managed by the Hong Kong International Theme Parks.</h5>
-                                <button class="btn btn-success">Read More</button>
+                                <button class="btn btn-default" data-toggle="modal" data-target="#disneyland">Read More</button>
                                 <button class="btn btn-success">Choose</button>
                             </div>
                             <div class="col-md-4 col-sm-4 col-xs-4">
                                 <img src="<?php echo base_url(); ?>assets/images/destinasi/greatwall.jpg" class="img-responsive" alt="125x125">
                                 <h2>Great Wall - China</h2>
                                 <h5>The Great Wall of China is a series of fortifications made of stone, brick, tamped earth, wood, and other materials, generally built along an east-to-west line across the historical northern borders of China to protect the Chinese states and empires against the raids and invasions of the various nomadic groups of the Eurasian Steppe.</h5>
-                                <button class="btn btn-success">Read More</button>
+                                <button class="btn btn-default" data-toggle="modal" data-target="#greatwall">Read More</button>
                                 <button class="btn btn-success">Choose</button>
                             </div>
                         </div>
@@ -68,3 +70,171 @@
     <!-- end x_panel -->
 </div>
 <!-- end col -->
+
+<!-- modal Paris Menara Eiffel -->
+<div class="modal fade" tabindex="-1" role="dialog" id="paris">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title">Paris (Eiffel Tower)</h4>
+                    </div>
+                    
+                    <div class="modal-body">
+                        <div class="container">
+                            <div class="col-sm-12" style="padding: 10px">
+                                <img src="<?php echo base_url(); ?>assets/images/paris.jpg" class="img-responsive" alt="125x125">
+                                
+                                <h5>A town which has Eiffel Tower as an icon of Paris. It has many spot for photograpy.The Eiffel Tower is a wrought iron lattice tower on the Champ de Mars in Paris, France. It is named after the engineer Gustave Eiffel, whose company designed and built the tower. </h5>
+                                <h7>source : wikipedia.com</h7>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>                        
+                    </div>
+                </div><!-- /.modal-content -->
+            </div><!-- /.modal-dialog -->
+        </div><!-- /.modal -->
+
+<!-- modal Universal Studio -->
+<div class="modal fade" tabindex="-1" role="dialog" id="universal">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title">Universal Studio - Singapore</h4>
+                    </div>
+                    
+                    <div class="modal-body">
+                        <div class="container">
+                            <div class="col-sm-12" style="padding: 10px">
+                                <img src="<?php echo base_url(); ?>assets/images/destinasi/universal.jpg" class="img-responsive" alt="125x125">
+                                
+                                <h5>Universal Studios Singapore is 20 hectares (49 acres) in size, occupying the easternmost part of the 49-hectare (120-acre) Resorts World Sentosa. There are a total of 24 attractions, of which 18 are original or specially adapted for the park. The park consists of seven themed zones which surround a lagoon.</h5>
+                                <h7>source : wikipedia.com</h7>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>                        
+                    </div>
+                </div><!-- /.modal-content -->
+            </div><!-- /.modal-dialog -->
+        </div><!-- /.modal -->
+
+<!-- modal Bali Indonesia -->
+<div class="modal fade" tabindex="-1" role="dialog" id="bali">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title">Bali - Indonesia</h4>
+                    </div>
+                    
+                    <div class="modal-body">
+                        <div class="container">
+                            <div class="col-sm-12" style="padding: 10px">
+                                <img src="<?php echo base_url(); ?>assets/images/destinasi/bali.jpg" class="img-responsive" alt="125x125">
+                                
+                                <h5>Bali is an Indonesian island known for its forested volcanic mountains, iconic rice paddies, beaches and coral reefs. The island is home to religious sites such as cliffside Uluwatu Temple. To the south, the beachside city of Kuta has lively bars, while Seminyak, Sanur and Nusa Dua are popular resort towns. The island is also known for its yoga and meditation retreats</h5>
+                                <h7>source : wikipedia.com</h7>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>                        
+                    </div>
+                </div><!-- /.modal-content -->
+            </div><!-- /.modal-dialog -->
+        </div><!-- /.modal -->
+
+<!-- modal Borobudur -->
+<div class="modal fade" tabindex="-1" role="dialog" id="borobudur">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title">Candi Borobudur - Indonesia</h4>
+                    </div>
+                    
+                    <div class="modal-body">
+                        <div class="container">
+                            <div class="col-sm-12" style="padding: 10px">
+                                <img src="<?php echo base_url(); ?>assets/images/destinasi/borobudur.jpg" class="img-responsive" alt="125x125">
+                                
+                                <h5>Borobudur, or Barabudur, is a 9th-century Mahayana Buddhist Temple in Magelang, Central Java, Indonesia. The monument consists of nine stacked platforms, six square and three circular, topped by a central dome.</h5>
+                                <h7>source : wikipedia.com</h7>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>                        
+                    </div>
+                </div><!-- /.modal-content -->
+            </div><!-- /.modal-dialog -->
+        </div><!-- /.modal -->
+
+<!-- modal disneyland hongkong -->
+<div class="modal fade" tabindex="-1" role="dialog" id="disneyland">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title">Disneyland - Hongkong</h4>
+                    </div>
+                    
+                    <div class="modal-body">
+                        <div class="container">
+                            <div class="col-sm-12" style="padding: 10px">
+                                <img src="<?php echo base_url(); ?>assets/images/destinasi/disneyland.jpg" class="img-responsive" alt="125x125">
+                                
+                                <h5>Hong Kong Disneyland is a theme park located on reclaimed land in Penny's Bay, Lantau Island. It is the first theme park located inside the Hong Kong Disneyland Resort and is owned and managed by the Hong Kong International Theme Parks.</h5>
+                                <h7>source : wikipedia.com</h7>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>                        
+                    </div>
+                </div><!-- /.modal-content -->
+            </div><!-- /.modal-dialog -->
+        </div><!-- /.modal -->
+
+<!-- modal greatwall China -->
+<div class="modal fade" tabindex="-1" role="dialog" id="greatwall">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title">Great Wall - China</h4>
+                    </div>
+                    
+                    <div class="modal-body">
+                        <div class="container">
+                            <div class="col-sm-12" style="padding: 10px">
+                                <img src="<?php echo base_url(); ?>assets/images/destinasi/greatwall.jpg" class="img-responsive" alt="125x125">
+                                
+                                <h5>The Great Wall of China is a series of fortifications made of stone, brick, tamped earth, wood, and other materials, generally built along an east-to-west line across the historical northern borders of China to protect the Chinese states and empires against the raids and invasions of the various nomadic groups of the Eurasian Steppe.</h5>
+                                <h7>source : wikipedia.com</h7>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>                        
+                    </div>
+                </div><!-- /.modal-content -->
+            </div><!-- /.modal-dialog -->
+        </div><!-- /.modal -->

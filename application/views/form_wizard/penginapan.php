bbdc713da77886f715d116f85ba707295e6a5e27
haidardzaky <haidardz@gmail.com>
2016-08-11 02:31:37 +0700

    update tampilan penginapan
    cekcek

--- application/views/form_wizard/penginapan.php
+++ application/views/form_wizard/penginapan.php
@@ -20,15 +20,24 @@
         <div class="container">
             <form class="form-horizontal" role="form">
                 <div class="form-group">
-                    <label class="col-sm-6 control-label">Check In</label>
-					<label class="col-sm-6 control-label">Check Out</label>
+					<label class="col-sm-4 control-label">Jenis Kamar</label>
+                    <label class="col-sm-4 control-label">Check In</label>
+					<label class="col-sm-4 control-label">Check Out</label>
 				</div>
-                <div class="col-sm-6">
+				<div class="col-md-4 col-sm-4 col-xs-12 form-group has-feedback">
+                <select class="form-control">
+                    <option>deluxe</option>
+                    <option>deluxe</option>
+                    <option>deluxe</option>
+                    <option>deluxe</option>
+                </select>
+            </div>
+                <div class="col-sm-4">
                     <input style="padding-left: 40px;" type="text" class="form-control has-feedback-left" id="single_cal4" placeholder="MM/DD/YYYY" aria-describedby="inputSuccess2Status4">
                     <span class="fa fa-calendar-o form-control-feedback left" aria-hidden="true"></span>
                     <span id="inputSuccess2Status4" class="sr-only">(success)</span>
                 </div>
-				<div class="col-sm-6">
+				<div class="col-sm-4">
                     <input style="padding-left: 40px;" type="text" class="form-control has-feedback-left" id="single_cal4" placeholder="MM/DD/YYYY" aria-describedby="inputSuccess2Status4">
                     <span class="fa fa-calendar-o form-control-feedback left" aria-hidden="true"></span>
                     <span id="inputSuccess2Status4" class="sr-only">(success)</span>
